d489a50705802798ea12b825875f56fd54970cee
FSOsuit <druzhba2@inbox.lv>
2023-02-20 21:13:57 +0200

    initial experiments

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book() {
+    //The constructor
+}
+
+function addBookToLibrary() {
+    
+};
+/* works outside the function, because function isn't called
+const submit = document.getElementById("submit");
+    submit.addEventListener('click', () => {
+        console.log("You pressed submit");
+});*/   
