93df5fc71bd3a82748fe806e14f16d82cac64c2e
FSOsuit <druzhba2@inbox.lv>
2023-02-21 21:03:47 +0200

    added event listener to submit button to create a new instance of the object

--- scripts.js
+++ scripts.js
@@ -1,14 +1,31 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
     //The constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
 }
+
+
 
 function addBookToLibrary() {
     
 };
-/* works outside the function, because function isn't called
-const submit = document.getElementById("submit");
-    submit.addEventListener('click', () => {
-        console.log("You pressed submit");
-});*/   
+
+
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+let read = document.getElementById("read");
+
+let submit = document.getElementById("submit");
+
+//creates new instance of the book after pressing submit button
+submit.addEventListener('click', () => {
+    const book = new Book(title.value, author.value, pages.value, read.value);
+    console.log(book);
+})
+
