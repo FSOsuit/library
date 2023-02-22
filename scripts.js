350d0e0c24f95b03579932a1f0e99ff1f125e74a
FSOsuit <druzhba2@inbox.lv>
2023-02-22 21:05:03 +0200

    added function that loops through library and displays books

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-
 }
 
 
@@ -15,15 +14,27 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 };
 
-
+//function to loop through library and display book.
+function displayBook(array) {
+    for (let book in array) { // loop through myLibrary
+        const book1 = array[book]; //store object of array
+        for (const property in book1){ //loop through the object 
+            let titleBook1 = `${book1[property]}`; // create variable of the value of the object
+            let bookTitle = document.createElement('div'); // create new div element for object of myLibrary
+            document.body.appendChild(bookTitle) // append the element to the body
+            bookTitle.textContent = titleBook1; // add text content to div
+        } // !!!MUST READ !!!displays books, but everytime submit is pressed loops through myLibrary from the begining.!!!!
+    }
+}
+//declaring variables for constructor
 let title;
 let author;
 let pages;
 let read;
 
-let submit = document.getElementById("submit");
 
 //creates new instance of the book after pressing submit button
+let submit = document.getElementById("submit");
 submit.addEventListener('click', () => {
     title = document.getElementById("title").value;
     author = document.getElementById("author").value;
@@ -31,6 +42,7 @@ submit.addEventListener('click', () => {
     read = document.getElementById("read").value;
     const book = new Book(title, author, pages, read);
     addBookToLibrary(book);
+    displayBook(myLibrary);
 })
 
 
